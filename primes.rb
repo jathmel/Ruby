ffce415b5f3066175820b392499e4cebff233603
jathmel <jathmel@gmail.com>
2018-11-11 19:52:51 -0500

    change

--- primes.rb
+++ primes.rb
@@ -1,23 +1,23 @@
-# primes.rb
+primes.rb
 
-# primes = []
-#
-# num = 1
-#
-# while primes.count < 100
-#   is_prime = true
-#   (1..num).each do |idx|
-#     if num % idx == 0
-#       is_prime = false
-#       break
-#     end
-#   end
-#
-#   if is_prime
-#     primes << num
-#   end
-# end
-# puts primes
+primes = []
+
+num = 1
+
+while primes.count < 100
+  is_prime = true
+  (1..num).each do |idx|
+    if num % idx == 0
+      is_prime = false
+      break
+    end
+  end
+
+  if is_prime
+    primes << num
+  end
+end
+puts primes
 require 'byebug'
 
 def prime?(num)
